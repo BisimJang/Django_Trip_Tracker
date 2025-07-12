f0d494f817528542db25292abff313c42713f50f
BisimJang <jangjason27@gmail.com>
2025-07-12 22:34:07 +0100

    Added the _base.html file, filtered Trips in the trip_list view to show only the trips for the logged in user

--- config/urls.py
+++ config/urls.py
@@ -28,13 +28,15 @@ urlpatterns = [
 ]
 
 #accounts/login/ [names='login']
-#  accounts/logout/ [name='logout']
+#accounts/logout/ [name='logout']
 #accounts/password_change/ [name='password_change']
 #accounts/password_change/done/ [name=password_change_done']
 #accounts/password_reset/ [name='password_reset']
 #accounts/password_reset/done/ [name='password_reset_done']
 #accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm' ]
 #accounts/reset/done/ [names='password_reset_complete']
+# dante-321-super user
+# jay-!@#$1234-normal user
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
